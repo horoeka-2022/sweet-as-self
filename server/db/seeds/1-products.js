c95974ae2ec19f042b5ca6916f181cec6c9288b8
Billie Staples <billielstaples@gmail.com>
2022-11-17 17:37:49 +1300

    draft data added

--- server/db/seeds/1-products.js
+++ server/db/seeds/1-products.js
@@ -1,25 +1,68 @@
 exports.seed = (knex) => {
   return knex('products').insert([
+        
     {
       id: 1,
-      name: 'Sweet As Peanut Butter',
-      description:
-        'A true crunchy extravaganza - sure to delight even the most diehard smooth fans!',
-      country: 'New Zealand',
+        offering: 'Yoga',
+        description: "Flow, breathe, pose! ",
+        teacher: 'Helena',
+        image: 'Imges/Images/yoga.png',
+        time: '11:00AM',
+        date:'11/11/2022', 
+        location: 'Enspiral Dev Academy',
     },
     {
       id: 2,
-      name: 'Sweet As Guacamole',
-      description:
-        "Made from perfectly ripe avos and with a good dash of chilli, you'll be back for more!",
-      country: 'New Zealand',
+        offering: 'Dance',
+        description:  "Move your body",
+        teacher: 'Johnny', 
+        image: 'Imges/Images/dance.png',
+        time: '11:00PM',
+        date: '09/09/2022',
+        country: 'Enspiral Dev Academy',
     },
     {
       id: 3,
-      name: 'Sweet As Hummus',
-      description:
-        "Garlic to the max - we really outdid ourselves here. Don't breathe too close to anyone for a while!",
-      country: 'New Zealand',
+        offering: 'Voice',
+        description: "Breathe from your diaphram, unearth the voice inside you",
+        teacher:'Richard',
+        image:'Imges/Images/theatre.png',
+        time: '10:00AM',
+        date:'12/12/2022',
+        country: 'Enspiral Dev Academy',
+    },
+    {
+      id: 4,
+        offering: 'Meditation',
+        description:
+        "Get in touch with your silent self, where does your mind sit in stilness, where can we wander in the breath of life?",
+        teacher:'Ahmad',
+        image:'Imges/Images/meditation.png',
+        time: '09:00AM',
+        date:'10/12/2022',
+        country: 'Enspiral Dev Academy',
+    },
+    {
+      id: 5,
+        offering: 'Cooking',
+        description:
+        "Nourish your body, nourish your mind",
+        teacher:'Clementine',
+        image:'Imges/Images/cooking.png',
+        time: '05:00PM',
+        date:'09/12/2022',
+        country: 'Enspiral Dev Academy',
+    },
+    {
+      id: 6,
+        offering: 'Journalling',
+        description:
+        "Observe not absorb, go on write about it",
+        teacher:'Ana',
+        image:'Imges/Images/journaling.png',
+        time: '09:30AM',
+        date:'28/11/2022',
+        country: 'Enspiral Dev Academy',
     },
   ])
 }
