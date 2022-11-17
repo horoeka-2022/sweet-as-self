188be4fa8f060b4d4ccc720627293bae59d2b4e5
elvin <elvintern@gmail.com>
2022-11-18 11:30:12 +1300

    css updated

--- client/components/ProductList.jsx
+++ client/components/ProductList.jsx
@@ -26,20 +26,22 @@ function ProductList({ children }) {
     <div className="productlist">
       <div className="welcome">
         <p>
-Sweet as Self is a not for profit organisation providing wonderful self care workshops. We believe in wellbeing for all.
-Founded in 2022 we follow a pay what you can model
-Our eco system is a giving one, of skill sharing and donation. 
-Got a class you would like to share with the community, get in touch about gifting your time today info@sweetasself.co.nz
-Have a nominal amount of money you would like to donate, book into one of our gratitude based classes today
-</p>
+          Sweet as Self is a not for profit organisation providing wonderful
+          self care workshops. We believe in wellbeing for all. Founded in 2022
+          we follow a pay what you can model Our eco system is a giving one, of
+          skill sharing and donation. Got a class you would like to share with
+          the community, get in touch about gifting your time today
+          info@sweetasself.co.nz Have a nominal amount of money you would like
+          to donate, book into one of our gratitude based classes today
+        </p>
       </div>
       {children} {/* This holds the WaitIndicator (from App) */}
       {products.map((product) => {
         return (
           <ProductListItem
-            key={product.id}
-            product={product}
-            addToCart={addProductToCart}
+          // key={product.id}
+          // product={product}
+          // addToCart={addProductToCart}
           />
         )
       })}
