ef1666ab6e09942637c95461c39296214c0fd6cf
Billie Staples <billielstaples@gmail.com>
2022-11-17 20:54:30 +1300

    descriptions better

--- server/db/seeds/1-products.js
+++ server/db/seeds/1-products.js
@@ -4,7 +4,7 @@ exports.seed = (knex) => {
     {
       id: 1,
         offering: 'Yoga',
-        description: "Flow, breathe, pose! ",
+        description: "Flow into nourishing postures. Breathe new life into your body. Get ready, now pose! Learn how to use breathe and alignment to truly ground yourself. This is an open class which is open to all levels. ",
         teacher: 'Helena',
         image: 'Imges/Images/yoga.png',
         time: '11:00AM',
@@ -14,7 +14,7 @@ exports.seed = (knex) => {
     {
       id: 2,
         offering: 'Dance',
-        description:  "Move your body",
+        description:  "Dont take off your dancing shoes, theres more than one thing on your mind. The most pertinenet being this jiving workshop! I wana see all you componenets and slices on the d floor with Johnny Tucci. We will laugh, sweat and (happy) cry. Lets feel the rhythm! Move your body!",
         teacher: 'Johnny', 
         image: 'Imges/Images/dance.png',
         time: '11:00PM',
@@ -24,8 +24,8 @@ exports.seed = (knex) => {
     {
       id: 3,
         offering: 'Voice',
-        description: "Breathe from your diaphram, unearth the voice inside you",
-        teacher:'Richard',
+        description: "Join Rich for an expiremtal workshop in intuitive voice. Breathe from your diaphram, unearth the voice inside you. Content warning: may contain riddles",
+        teacher:'Rich',
         image:'Imges/Images/theatre.png',
         time: '10:00AM',
         date:'12/12/2022',
@@ -35,7 +35,7 @@ exports.seed = (knex) => {
       id: 4,
         offering: 'Meditation',
         description:
-        "Get in touch with your silent self, where does your mind sit in stilness, where can we wander in the breath of life?",
+        "Get in touch with your silent self, how does your soul sit in stilness, where can we wander in the breath of life?",
         teacher:'Ahmad',
         image:'Imges/Images/meditation.png',
         time: '09:00AM',
@@ -46,7 +46,7 @@ exports.seed = (knex) => {
       id: 5,
         offering: 'Cooking',
         description:
-        "Nourish your body, nourish your mind",
+        "Nourish your body, nourish your mind. Get out those pots and pans, we are firing up the elements. Cool, calm and collected Clem will teach you the ways of the kitchen in this immersive experience.",
         teacher:'Clementine',
         image:'Imges/Images/cooking.png',
         time: '05:00PM',
@@ -55,13 +55,24 @@ exports.seed = (knex) => {
     },
     {
       id: 6,
-        offering: 'Journalling',
+        offering: 'Journaling',
         description:
-        "Observe not absorb, go on write about it",
+        "Observe not absorb, go on write about it. Articulate your thoughts and feelings. ",
         teacher:'Ana',
         image:'Imges/Images/journaling.png',
         time: '09:30AM',
         date:'28/11/2022',
+        country: 'Enspiral Dev Academy',
+    },
+    {
+      id: 7,
+        offering: '',
+        description:
+        "",
+        teacher:'Alex',
+        image:'Imges/Images/.png',
+        time: '0AM',
+        date:'2022',
         country: 'Enspiral Dev Academy',
     },
   ])
